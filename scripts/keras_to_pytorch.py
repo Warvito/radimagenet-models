42463dbf6124f2f04fdc35f06950da3f9510d433
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2022-12-20 13:29:58 +0000

    initial commit

--- scripts/keras_to_pytorch.py
+++ scripts/keras_to_pytorch.py
@@ -26,63 +26,72 @@ def convert_bn(pytorch_bn, tf_bn):
     return pytorch_bn
 
 
+def convert_stack(pytorch_stack, keras_model, stack_name, num_blocks):
+    layers_list = []
+    for layer in keras_model.layers:
+        if stack_name in layer.get_config()['name']:
+            layers_list.append(layer)
+
+    for i in range(1, num_blocks+1):
+        pytorch_block = pytorch_stack[i-1]
+        for layer in layers_list:
+            if f"{stack_name}_block{str(i)}_0_conv" in layer.get_config()['name']:
+                pytorch_block.downsample[0] = convert_conv(pytorch_block.downsample[0], layer)
+            if f"{stack_name}_block{str(i)}_0_bn" in layer.get_config()['name']:
+                pytorch_block.downsample[1] = convert_bn(pytorch_block.downsample[1], layer)
+
+            if f"{stack_name}_block{str(i)}_1_conv" in layer.get_config()['name']:
+                pytorch_block.conv1 = convert_conv(pytorch_block.conv1, layer)
+            if f"{stack_name}_block{str(i)}_1_bn" in layer.get_config()['name']:
+                pytorch_block.bn1 = convert_bn(pytorch_block.bn1, layer)
+            if f"{stack_name}_block{str(i)}_2_conv" in layer.get_config()['name']:
+                pytorch_block.conv2 = convert_conv(pytorch_block.conv2, layer)
+            if f"{stack_name}_block{str(i)}_2_bn" in layer.get_config()['name']:
+                pytorch_block.bn2 = convert_bn(pytorch_block.bn2, layer)
+            if f"{stack_name}_block{str(i)}_3_conv" in layer.get_config()['name']:
+                pytorch_block.conv3 = convert_conv(pytorch_block.conv3, layer)
+            if f"{stack_name}_block{str(i)}_3_bn" in layer.get_config()['name']:
+                pytorch_block.bn3 = convert_bn(pytorch_block.bn3, layer)
+
+    return pytorch_stack
+
+
 pytorch_model = ResNet50()
 pytorch_model.eval()
-tf_keras_model = tf.keras.models.load_model(input_path)
+keras_model = tf.keras.models.load_model(input_path)
 
-pytorch_model.conv1 = convert_conv(pytorch_model.conv1, tf_keras_model.get_layer("conv1_conv"))
-pytorch_model.bn1 = convert_bn(pytorch_model.bn1, tf_keras_model.get_layer("conv1_bn"))
+pytorch_model.conv1 = convert_conv(pytorch_model.conv1, keras_model.get_layer("conv1_conv"))
+pytorch_model.bn1 = convert_bn(pytorch_model.bn1, keras_model.get_layer("conv1_bn"))
 
-conv_name = "conv2_"
-num_blocks = 3
-layers_list = []
-for layer in tf_keras_model.layers:
-    if conv_name in layer.get_config()['name']:
-        layers_list.append(layer)
+pytorch_model.layer1 = convert_stack(pytorch_model.layer1, keras_model, "conv2", num_blocks=3)
+pytorch_model.layer2 = convert_stack(pytorch_model.layer2, keras_model, "conv3", num_blocks=4)
+pytorch_model.layer3 = convert_stack(pytorch_model.layer3, keras_model, "conv4", num_blocks=6)
+pytorch_model.layer4 = convert_stack(pytorch_model.layer4, keras_model, "conv5", num_blocks=3)
 
-pytorch_stack = pytorch_model.layer1
-for i in range(1, num_blocks+1):
-    pytorch_block = pytorch_stack[i-1]
-    for layer in layers_list:
-        print(layer.get_config()['name'])
-        if f"{conv_name}block{str(i)}_0_conv" in layer.get_config()['name']:
-            pytorch_block.downsample[0] = convert_conv(pytorch_block.downsample[0], layer)
-        if f"{conv_name}block{str(i)}_0_bn" in layer.get_config()['name']:
-            pytorch_block.downsample[1] = convert_bn(pytorch_block.downsample[1], layer)
-
-        if f"{conv_name}block{str(i)}_1_conv" in layer.get_config()['name']:
-            pytorch_block.conv1 = convert_conv(pytorch_block.conv1, layer)
-        if f"{conv_name}block{str(i)}_1_bn" in layer.get_config()['name']:
-            pytorch_block.bn1 = convert_bn(pytorch_block.bn1, layer)
-        if f"{conv_name}block{str(i)}_2_conv" in layer.get_config()['name']:
-            pytorch_block.conv2 = convert_conv(pytorch_block.conv2, layer)
-        if f"{conv_name}block{str(i)}_2_bn" in layer.get_config()['name']:
-            pytorch_block.bn2 = convert_bn(pytorch_block.bn2, layer)
-        if f"{conv_name}block{str(i)}_3_conv" in layer.get_config()['name']:
-            pytorch_block.conv3 = convert_conv(pytorch_block.conv3, layer)
-        if f"{conv_name}block{str(i)}_3_bn" in layer.get_config()['name']:
-            pytorch_block.bn3 = convert_bn(pytorch_block.bn3, layer)
-#
-# pytorch_model.layer1[0].get_submodule("bn1")
-#
-# pytorch_model.conv1.weight.data = torch.tensor(np.transpose(tf_keras_model.get_layer("conv1_conv").kernel.numpy(), (3, 2, 0, 1)))
-# pytorch_model.conv1.bias.data = torch.tensor(tf_keras_model.get_layer("conv1_conv").bias.numpy())
-#
-# pytorch_model.bn1.weight.data = torch.tensor(tf_keras_model.get_layer("conv1_bn").gamma.numpy())
-# pytorch_model.bn1.bias.data = torch.tensor(tf_keras_model.get_layer("conv1_bn").beta.numpy())
-# pytorch_model.bn1.running_mean.data = torch.tensor(tf_keras_model.get_layer("conv1_bn").moving_mean.numpy())
-# pytorch_model.bn1.running_var.data = torch.tensor(tf_keras_model.get_layer("conv1_bn").moving_variance.numpy())
 
 torch.set_printoptions(precision=10)
+pytorch_model.eval()
 with torch.no_grad():
     x_pt = torch.ones(1,3,224,224)
-    outputs_pt = pytorch_model.conv1(x_pt)
-    outputs_pt = pytorch_model.bn1(outputs_pt)
+    outputs_pt = pytorch_model(x_pt)
+    outputs_pt = np.transpose(outputs_pt.numpy(), (0, 2, 3, 1))
 
-x_tf = tf.ones((1,224,224, 3))
-outputs_tf = tf_keras_model.get_layer("conv1_pad")(x_tf)
-outputs_tf = tf_keras_model.get_layer("conv1_conv")(outputs_tf)
-outputs_tf = tf_keras_model.get_layer("conv1_bn")(outputs_tf)
-outputs_tf = np.transpose(outputs_tf.numpy(), (0, 3, 1, 2))
+x_tf = tf.ones((1, 224,224, 3))
+outputs_tf = keras_model(x_tf, training=False)
+# outputs_tf = keras_model.get_layer("conv1_conv")(outputs_tf)
+# outputs_tf = keras_model.get_layer("conv1_bn")(outputs_tf)
+# outputs_tf = np.transpose(outputs_tf.numpy(), (0, 3, 1, 2))
 
-np.allclose(outputs_tf, outputs_pt.numpy(), atol=1e-06)
+print(np.allclose(outputs_tf.numpy(), outputs_pt, atol=1e-06))
+
+
+
+with torch.no_grad():
+    x_pt = torch.ones((1, 64, 112, 112))
+    pt_layer = pytorch_model.bn1.eval()
+    out_pt = pt_layer(x_pt)
+    out_pt = np.transpose(out_pt.numpy(), (0, 2, 3, 1))
+
+bn_layer = keras_model.get_layer("conv1_bn")
+x_tf = tf.ones((1, 112, 112, 64))
+out_tf = bn_layer(x_tf, training=False)
